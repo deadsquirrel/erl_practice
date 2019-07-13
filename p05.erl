cd96fca5724485c25bc12aa1f91f9cb42e9cf89e
yanki <deadsquirrel@users.noreply.github.com>
2019-07-13 18:51:22 +0300

    Add files via upload
    added tests

--- p05.erl
+++ p05.erl
@@ -1,6 +1,11 @@
 -module(p05).
+-include_lib("eunit/include/eunit.hrl").
 -export([reverse/1]).
 
+reverse([]) ->
+    [];
+reverse([H|[]]) ->
+    [H];
 reverse([H|T]) ->
 	reverse(T,[H]).
 
@@ -9,3 +14,6 @@ reverse([H|[]],L) ->
 reverse([H|T],L) ->
 	reverse(T,[H|L]).
 
+reverse_nil_test() -> [] = p05:reverse([]).
+reverse_one_test() -> [1] = p05:reverse([1]).
+reverse_two_test() -> [2,1] = p05:reverse([1,2]).
