5bc631722d5e09cf6e7c5a11ddcd24a935c04302
yanki <deadsquirrel@users.noreply.github.com>
2019-07-21 23:39:02 +0300

    added update

--- compare.erl
+++ compare.erl
@@ -1,5 +1,6 @@
 -module(compare).
--export([create/0]).
+-export([create/0,
+		update/0]).
 		
 create() ->
 	{Time4Map, V1} = timer:tc(fun() -> crmap(), create_map end),
@@ -10,6 +11,29 @@ create() ->
 	io:format("~p s -- ~p~n", [Time4dict, V3]),
 	{Time4ets, V4} = timer:tc(fun() -> crets(), create_ets end),
 	io:format("~p s -- ~p~n", [Time4ets, V4]).
+
+update() ->
+	{Time4Map, V10} = timer:tc(fun() -> updmap(), update_map end),
+	io:format("~p s -- ~p~n", [Time4Map, V10]),
+	{Time4prop, V20} = timer:tc(fun() -> updprop(), update_prop end),
+	io:format("~p s -- ~p~n", [Time4prop, V20]),
+	{Time4dict, V30} = timer:tc(fun() -> upddict(), update_dict end),
+	io:format("~p s -- ~p~n", [Time4dict, V30]),
+	{Time4ets, V40} = timer:tc(fun() -> updets(), update_ets end),
+	io:format("~p s -- ~p~n", [Time4ets, V40]).
+
+updmap()->
+	repeats(maps:new(), 10000).
+
+updprop()->
+	repeats([{key1,val1}|{}], 10000).
+	
+upddict()->
+	repeats(dict:store(key1,"value1",dict:new()), 10000).
+	
+updets()->
+	repeats(ets:new(my_ets, []), 10000).
+
 
 crmap()->
 	repeats(maps:new(), 10000).
